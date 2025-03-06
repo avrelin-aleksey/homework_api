4ec80a9bc977acb3e93bcb0bfb411bab5bb423f0
Avrelin Aleksey <aavrelin@wellink.ru>
2025-03-07 01:13:11 +0300

    Добавлены проверки в test_memes.py

--- homework_24/test/test_memes.py
+++ homework_24/test/test_memes.py
@@ -19,13 +19,41 @@ class TestGetMemes:
     @allure.story("Получение мема по существующему id")
     def test_get_memes_by_id(self, memes_endpoint, token, create_memes_for_test):
         response = memes_endpoint.get_memes_by_id(create_memes_for_test, token)
-        memes_endpoint.check_status_code(200)
-        assert response["id"] == create_memes_for_test
+        with allure.step("Проверка статус код 200"):
+            memes_endpoint.check_status_code(200)
+        with allure.step("Полученный id соответствует id при создании"):
+            assert response["id"] == create_memes_for_test
 
     @allure.story("Получение мема с несуществующим id")
     def test_get_memes_invalid_id(self, memes_endpoint, token):
         memes_endpoint.get_memes_by_id(99999999, token)
-        memes_endpoint.check_status_code(404)
+        with allure.step("Проверка статус код = 404"):
+            memes_endpoint.check_status_code(404)
+        with allure.step("Приходит в ответе текст '404 Not Found'"):
+            assert "404 Not Found" in memes_endpoint.response.text
+
+    @allure.story("Получение всех мемов без авторизации по токену")
+    def test_get_all_memes_without_auth(self, memes_endpoint):
+        memes_endpoint.get_all_memes(None)
+        with allure.step("Проверка статус код = 401"):
+            memes_endpoint.check_status_code(401)
+        with allure.step("Приходит в ответе текст '401 Unauthorized'"):
+            assert "401 Unauthorized" in memes_endpoint.response.text
+
+    import allure
+
+    @allure.story("Получение всех мемов c авторизацией по токену")
+    def test_get_memes_all(self, memes_endpoint, token):
+        memes = memes_endpoint.get_all_memes(token)
+        with allure.step("Проверка статус код = 200"):
+            memes_endpoint.check_status_code(200)
+        with allure.step("Проверка, что список мемов не пуст"):
+            assert len(memes) > 0, "Список мемов пуст"
+        with allure.step("Проверка, что в ответе присутствует ключ 'data'"):
+            assert "data" in memes, "Ответ не содержит ключ 'data'"
+        with allure.step("Проверка, что количество мемов больше 2000"):
+            memes_all_count = len(memes["data"])
+            assert memes_all_count > 2000, f"Ожидалось, что мемов будет больше 2000, а их {memes_all_count}"
 
 
 @allure.feature("Создание мемов")
@@ -33,24 +61,35 @@ class TestCreateMemes:
     @allure.story("Успешное создание мема")
     def test_create_memes(self, memes_endpoint, token):
         response = memes_endpoint.create_memes(token, **memes_data)
-        memes_endpoint.check_status_code(200)
-        assert response["id"] is not None
+        with allure.step("Проверка статус код = 200"):
+            memes_endpoint.check_status_code(200)
+        with allure.step("Проверка наличия id"):
+            assert response["id"] is not None
 
     @allure.story("Создание мема без авторизации")
     def test_create_memes_without_auth(self, memes_endpoint):
         memes_endpoint.create_memes(None, **memes_data)
-        memes_endpoint.check_status_code(401)
+        with allure.step("Проверка статус код = 401"):
+            memes_endpoint.check_status_code(401)
+        with allure.step("Приходит в ответе текст '401 Unauthorized'"):
+            assert "401 Unauthorized" in memes_endpoint.response.text
 
     @allure.story("Создание мема с пустыми значениями полей")
     def test_create_memes_empty_fields(self, memes_endpoint, token):
         memes_endpoint.create_memes(token, **empty_value)
-        memes_endpoint.check_status_code(400) # приходит 200
+        with allure.step("Проверка статус код = 400"):
+            memes_endpoint.check_status_code(400) # приходит 200, но поидее должна быть ошибка
+        with allure.step("Приходит в ответе текст 'Bad Request'"):
+            assert "Bad Request" in memes_endpoint.response.text
+
 
     @allure.story("Отправка post-запроса с существующим id")
     def test_create_memes_with_existing_id(self, memes_endpoint, token):
         response = memes_endpoint.create_memes(token, **memes_data_post_id)
-        memes_endpoint.check_status_code(200)
-        assert response["id"] is not None
+        with allure.step("Проверка статус код = 200"):
+            memes_endpoint.check_status_code(200)
+        with allure.step("Проверка наличия id"):
+            assert response["id"] is not None
 
     @allure.story("Создание мема c невалидными body")
     @pytest.mark.parametrize('test_case', [
@@ -61,17 +100,24 @@ class TestCreateMemes:
         memes_data_negative5])
     def test_create_memes_negative(self, memes_endpoint, token, test_case):
         memes_endpoint.create_memes(token, **test_case)
-        memes_endpoint.check_status_code(400)
+        with allure.step("Проверка статус код = 400"):
+            memes_endpoint.check_status_code(400)
+        with allure.step("Приходит в ответе текст 'Bad Request'"):
+            assert "Bad Request" in memes_endpoint.response.text
 
     @allure.story("Создание мема с слишком длинным текстом")
     def test_create_memes_long_text(self, memes_endpoint, token):
         memes_endpoint.create_memes(token, **long_text)
-        memes_endpoint.check_status_code(200) # тут не понятно должно ли быть 400 или 200
+        with allure.step("Проверка статус код = 200"):
+            memes_endpoint.check_status_code(200) # тут не понятно должно ли быть 400 или 200
 
     @allure.story("Создание мема с невалидным tags")
     def test_create_memes_invalid_tags(self, memes_endpoint, token):
         memes_endpoint.create_memes(token, **invalid_tags)
-        memes_endpoint.check_status_code(400)
+        with allure.step("Проверка статус код = 400"):
+            memes_endpoint.check_status_code(400)
+        with allure.step("Приходит в ответе текст 'Bad Request'"):
+            assert "Bad Request" in memes_endpoint.response.text
 
 
 @allure.feature("Изменение мемов")
@@ -86,8 +132,10 @@ class TestUpdateMemes:
             "info": {"key": "updated_value"},
         }
         response = memes_endpoint.update_memes(create_memes_for_test, token, **updated_data)
-        memes_endpoint.check_status_code(200)
-        assert response["text"] == "Updated Meme"
+        with allure.step("Проверка статус код = 200"):
+            memes_endpoint.check_status_code(200)
+        with allure.step("Приходит в ответе текст 'Updated Meme'"):
+            assert response["text"] == "Updated Meme"
 
     @allure.story("PUT изменение мема с невалидными данными")
     @pytest.mark.parametrize('test_case', [
@@ -98,8 +146,10 @@ class TestUpdateMemes:
         memes_data_negative5])
     def test_update_memes_negative(self, memes_endpoint, token, create_memes_for_test, test_case):
         memes_endpoint.update_memes(create_memes_for_test, token, **test_case)
-        memes_endpoint.check_status_code(400)
-        assert "Bad Request" in memes_endpoint.response.text
+        with allure.step("Проверка статус код = 400"):
+            memes_endpoint.check_status_code(400)
+        with allure.step("Приходит в ответе текст 'Bad Request'"):
+            assert "Bad Request" in memes_endpoint.response.text
 
     @allure.story("Обновление мема без авторизации")
     def test_update_memes_without_auth(self, memes_endpoint, create_memes_for_test):
@@ -111,7 +161,10 @@ class TestUpdateMemes:
             "info": {"key": "updated_value"},
         }
         memes_endpoint.update_memes(create_memes_for_test, token=None, **updated_data)
-        memes_endpoint.check_status_code(401)
+        with allure.step("Проверка статус код = 401"):
+            memes_endpoint.check_status_code(401)
+        with allure.step("Приходит в ответе текст '401 Unauthorized'"):
+            assert "401 Unauthorized" in memes_endpoint.response.text
 
 
 @allure.feature("Удаление мемов")
@@ -119,20 +172,32 @@ class TestDeleteMemes:
     @allure.story("Удаление существующего мема")
     def test_delete_memes(self, memes_endpoint, token, create_memes_for_test):
         memes_endpoint.delete_memes(create_memes_for_test, token)
-        memes_endpoint.check_status_code(200)
+        with allure.step("Проверка статус код = 200"):
+            memes_endpoint.check_status_code(200)
 
     @allure.story("Удаление несуществующего мема")
     def test_delete_memes_negative(self, memes_endpoint, token):
         memes_endpoint.delete_memes(111111111111111111, token)
-        memes_endpoint.check_status_code(404)
+        with allure.step("Проверка статус код = 404"):
+            memes_endpoint.check_status_code(404)
+        with allure.step("Приходит в ответе текст '404 Not Found'"):
+            assert "404 Not Found" in memes_endpoint.response.text
 
     @allure.story("Удаление мема дважды подряд")
     def test_delete_memes_twice(self, memes_endpoint, token, create_memes_for_test):
-        memes_endpoint.delete_memes(create_memes_for_test, token)
-        memes_endpoint.delete_memes(create_memes_for_test, token)
-        memes_endpoint.check_status_code(404)
+        with allure.step("Первая попытка удаления успешная"):
+            memes_endpoint.delete_memes(create_memes_for_test, token)
+        with allure.step("Вторая попытка удаления, не успешная"):
+            memes_endpoint.delete_memes(create_memes_for_test, token)
+        with allure.step("Проверка статус код = 404"):
+            memes_endpoint.check_status_code(404)
+        with allure.step("Приходит в ответе текст '404 Not Found'"):
+            assert "404 Not Found" in memes_endpoint.response.text
 
     @allure.story("Удаление существующего мема без токена")
     def test_delete_memes_without_token(self, memes_endpoint, create_memes_for_test):
         memes_endpoint.delete_memes_without_token(create_memes_for_test)
-        memes_endpoint.check_status_code(400) # но приходит ответ 200
+        with allure.step("Проверка статус код = 400"):
+            memes_endpoint.check_status_code(400) # но приходит ответ 200
+        with allure.step("Приходит в ответе текст 'Bad Request'"):
+            assert "Bad Request" in memes_endpoint.response.text
